441028c40399f7ea216f7e32dfe1f0a426e941ec
vidergold <iravidergold@yandex.ru>
2024-05-23 01:05:32 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -15,4 +15,13 @@ function showDate() {
     for (let [country, locale] of Object.entries(locales)) {
         localeDates.innerHTML += `Дата и время для локали ${country}: ${today.toLocaleString(locale)}<br>`;
     }
+
+    let additionalInfo = document.getElementById('additional-info');
+    let daysOfWeek = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+    additionalInfo.innerHTML = `
+        Текущий год: ${today.getFullYear()}<br>
+        Текущий месяц: ${today.toLocaleString('ru-RU', { month: 'long' })}<br>
+        Текущая дата: ${today.getDate()}<br>
+        День недели: ${daysOfWeek[today.getDay()]}
+    `;
 }
